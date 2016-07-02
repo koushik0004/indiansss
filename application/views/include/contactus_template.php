269d2067992b7d85cb6cad5dc1ae2e3203f9e5b8
koushik0004 <koushik.sadhukhan0004@gmail.com>
2016-07-02 10:43:27 +0530

    updating contact-us and home page

--- application/views/include/contactus_template.php
+++ application/views/include/contactus_template.php
@@ -10,23 +10,32 @@
                     <td align="left" valign="top">Email<br /></td>
                   </tr>
                   <tr>
-                    <td width="50%" align="left" valign="top"><p>Indian Journal of Spatial Science<br />
-                      Department of Geography,<br />
+                    <td width="50%" align="left" valign="top">
+                        <p>Editor-in-Chief <br />
+                            Indian Journal of Spatial Science<br />
+                        </p>    
+                            
+                    <!--  
+                        Department of Geography,<br />
                       Presidency University (formerly College),<br />
                       86/1, College Street,<br />
                       Kolkata - 700 073 <br />
                       Ph. : + 91 33 2241 1960 (Extn. 206) <br />
                       Fax: +91 33 2241 2738, +91 33 2257 2444</p>
 <p>or</p>
+-->
+                        
                       <p>&quot;Sumangal&quot;<br />
                         47 Bosepara Road<br />
                         P.O. – Chandannagar, <br />
                         Dist. – Hooghly<br />
                         West Bengal, India<br />
-                    PIN – 712 136</p></td>
+                        PIN – 712 136 <br />
+                        Phone No. +91 98 3655 2173<br />
+                          <span class="last-mob-number">+91 98 3682 2173</span></p>
+                      </td>
                     <td width="50%" align="left" valign="top"><p>editorijss2012@gmail.com</p>
 <p>profdrashis@gmail.com</p>
-                      <p>etraverse.ijss@gmail.com</p>
                     <p>etraverse.ijss@gmail.com</p></td>
                   </tr>
                 </table>
--- application/views/include/home_template.php
+++ application/views/include/home_template.php
@@ -40,7 +40,11 @@
             */ ?>
             <div id="middle_col">
               <div class="header-decoration overlap-padding">Welcome to our E-Journal</div>
+                <?php if(isset($tot_content) && $tot_content !== FALSE):?>
+                	<?php echo stripslashes($tot_content[0]->content); ?>
+                <?php else:?>
               <p>eTraverse, The Indian Journal of Spatial Science shall henceforth be known as the "Indian Journal of Spatial Science". It is a publication of the "Indian Society of Spatial Scientists" with Head Quarter currently housed at the "Department of Geography, Presidency University, 86/1 College Street, Kolkata, 700 073" and with permanent address at "SUMANGAL", 47 Bosepara Road, P.O. – Chandannagar, Dist. – Hooghly, West Bengal, India PIN – 712 136. It is concerned with all fields of geography, geology, social sciences, geoinformatics and other spatial sciences. It publishes "Indian Journal of Spatial Science" twice a year with Summer and Winter Issues each containing 6 or more Articles. It is primarily intended for the publication of research papers by the young and innovative scholars of geography and allied disciplines all over the world.</p>
+                <?php endif; ?>
               <p><br />
                 Editor-in-Chief<br />
               <img src="<?php echo base_url(); ?>assats/looks/images/signature.png" width="98" height="89" alt="Signature" /> <br />
